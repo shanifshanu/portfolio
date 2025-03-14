b70f7ac363ef41e182c3dedf3e269b935d79e5e2
Rif-7 <muhammadrifan974530@gmail.com>
2025-03-14 22:21:05 +0530

    initial commit

---
+++ src/components/layout.js
@@ -0,0 +1,22 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+const Layout = ({ location, title, children }) => {
+  const rootPath = `${__PATH_PREFIX__}/`
+  const isRootPath = location.pathname === rootPath
+  let header
+
+  if (isRootPath) {
+    header = (
+      <h1 className="main-heading">
+        <Link to="/">
+          Muhammed<br></br>Shanif
+        </Link>
+      </h1>
+    )
+  }
+
+  return <div className="global-wrapper" data-is-root-path={isRootPath}></div>
+}
+
+export default Layout
---
+++ src/pages/about.js
@@ -0,0 +1,112 @@
+import * as React from "react"
+import { graphql } from "gatsby"
+
+import Seo from "../components/seo"
+import Navbar from "../components/navbar"
+import Footer from "../components/footer"
+
+const About = ({ data, location }) => {
+  return (
+    <div>
+      <div>
+        <Navbar></Navbar>
+      </div>
+      <div className="body-global-wrapper">
+        <br></br>
+        <p className="titleimage">
+          Welcome to my portfolio! I’m Muhammed Shanif, an enthusiastic actor
+          with a passion for telling compelling stories through the art of
+          performance. Trained in various acting techniques, I thrive on
+          bringing characters to life, whether on stage or in front of the
+          camera. My journey in the performing arts is fueled by a love for
+          creativity and a commitment to honing my craft. Here, you’ll find a
+          collection of my work, including stage performances and film projects,
+          showcasing my versatility and dedication to the art of acting. Thank
+          you for visiting, and I look forward to sharing my artistic journey
+          with you!
+          <br></br>
+          <br></br>
+          <u>Education:</u>
+          <br></br>
+          BTech Electrical and Electronics Engineering (EEE)
+          <br></br>
+          THEJUS ENGINEERING COLLEGE
+          <br></br>
+          <br></br>
+          <u>Location:</u>
+          <br></br>
+          Edakkazhiyoor, Chavakkad, Thrissur, Kerala - 680515
+          <br></br>
+          <br></br>
+          <u>Personal Details:</u>
+          <br></br>
+          Age: 21
+          <br></br>
+          Height: 5'8"
+          <br></br>
+          Weight: 50kg
+          <br></br>
+          <br></br>
+          You can reach out to me via email or connect with me on Instagram.
+          <br></br>
+          <p>
+            Feel free to{" "}
+            <a
+              href="mailto:Shanifsharafudheen007@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ textDecoration: "underline", fontWeight: "bold" }}
+            >
+              send me an email
+            </a>{" "}
+            or connect with me on{" "}
+            <a
+              href="https://www.instagram.com/sh_anif313"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ textDecoration: "underline", fontWeight: "bold" }}
+            >
+              Instagram
+            </a>
+            .
+          </p>
+        </p>
+      </div>
+      <div>
+        <Footer></Footer>
+      </div>
+    </div>
+  )
+}
+
+export default About
+
+/**
+ * Head export to define metadata for the page
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
+ */
+export const Head = () => <Seo title="About" />
+
+export const pageQuery = graphql`
+  {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { frontmatter: { date: DESC } }) {
+      nodes {
+        excerpt
+        fields {
+          slug
+        }
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          title
+          description
+        }
+      }
+    }
+  }
+`
